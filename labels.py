184749a71f5ad1b09c018f7e9ff22774e2ba696c
Murilo Andrade <murilo.andrade@gmail.com>
2025-07-10 01:04:38 -0300

    get_labels function created to show names of class ids (english or portuguese)

---
+++ labels.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+# from translate import pt_br # uncomment this line if you need class names in portuguese
+
+def get_label(cls_id):
+    model = YOLO("models/yolov8n.pt")  # I am using nano just to get the class names, no detection is done here
+
+    # Lista das classes COCO
+    coco_classes = model.names  # dict {id: nome}
+    label = coco_classes[cls_id]
+    try:
+        pt_br = globals()["pt_br"]
+        if isinstance(pt_br, dict):
+            label = pt_br.get(coco_classes[cls_id],coco_classes[cls_id])
+    except KeyError:
+        label = coco_classes[cls_id]
+    return label.capitalize()
